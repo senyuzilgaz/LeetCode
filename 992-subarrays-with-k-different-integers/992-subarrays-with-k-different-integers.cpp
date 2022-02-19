50e0ce230adc10f179fedcb96136f94728c6ac48
Ilgaz Şenyüz <61624665+senyuzilgaz@users.noreply.github.com>
2022-02-19 16:45:28 +0300

    Time: 156 ms (62.52%), Space: 45.1 MB (75.10%) - LeetHub

--- 992-subarrays-with-k-different-integers/992-subarrays-with-k-different-integers.cpp
+++ 992-subarrays-with-k-different-integers/992-subarrays-with-k-different-integers.cpp
@@ -1,13 +1,33 @@
 class Solution {
 public:
 int subarraysWithKDistinct(vector<int>& A, int K, int res = 0) {
-  vector<int> m(A.size() + 1);
-  for(auto i = 0, j = 0, prefix = 0, cnt = 0; i < A.size(); ++i) {
-    if (m[A[i]]++ == 0) ++cnt;
-    if (cnt > K) --m[A[j++]], --cnt, prefix = 0;
-    while (m[A[j]] > 1) ++prefix, --m[A[j++]];
-    if (cnt == K) res += prefix + 1;
-  }
-  return res;
+    return atMostK(A, K) - atMostK(A, K-1);
+}
+    
+int atMostK(vector<int> &array, int K){
+    int result = 0;
+    int left = 0;
+    int uniqueIntegers = 0;
+    
+    unordered_map<int, int> count;
+    for(int right = 0; right < array.size(); ++right){
+        int newNumber = array[right];
+        if(count[newNumber] == 0){
+            uniqueIntegers ++;
+        }
+        count[newNumber]++;
+        while(uniqueIntegers > K){
+            int leftMostNumber = array[left];
+            count[leftMostNumber]--;
+            if(count[leftMostNumber] == 0){
+                uniqueIntegers--;
+            }
+            left++;
+        }
+        int lengthOfSubarray = right - left + 1;
+        result += lengthOfSubarray;
+    }
+    return result;
 }
 };
+

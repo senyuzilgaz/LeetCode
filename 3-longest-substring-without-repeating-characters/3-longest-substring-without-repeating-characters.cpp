044b147852e07a0bbd8e954bb2e231a1a237aa9e
Ilgaz Şenyüz <61624665+senyuzilgaz@users.noreply.github.com>
2022-02-19 10:37:29 +0300

    Time: 28 ms (45.83%), Space: 8.3 MB (57.25%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -1,24 +1,18 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        int len = s.length();
-        int longest = 0;
-        int current = 0;
         unordered_map<char, int> lastSeen;
+        int longest = 0;
         int startIndex = 0;
-        
-        if( len < 2)
-            return len;
-        
-        for(int i = 0; i < len; ++i){
+        for(int i = 0; i < s.length(); ++i){
             char ch = s[i];
             if(lastSeen.find(ch) != lastSeen.end()){
                 startIndex = max(startIndex, lastSeen[ch] + 1);
             }
-            longest = max(longest, i + 1 - startIndex);
+            
             lastSeen[ch] = i;
+            longest = max(longest, i - startIndex + 1);
         }
-        
-         return longest;
+        return longest;
     }
 };

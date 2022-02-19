f1a261f4918eb5c7594dc6b90507798cbd6360d7
Ilgaz Şenyüz <61624665+senyuzilgaz@users.noreply.github.com>
2022-02-19 10:23:54 +0300

    Time: 37 ms (66.64%), Space: 71.6 MB (11.22%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.cpp
+++ 2-add-two-numbers/2-add-two-numbers.cpp
@@ -11,16 +11,30 @@
 class Solution {
 public:
     ListNode *addTwoNumbers(ListNode *l1, ListNode *l2) {
-        ListNode preHead(0), *p = &preHead;
-        int extra = 0;
-        while (l1 || l2 || extra) {
-            int sum = (l1 ? l1->val : 0) + (l2 ? l2->val : 0) + extra;
-            extra = sum / 10;
-            p->next = new ListNode(sum % 10);
-            p = p->next;
-            l1 = l1 ? l1->next : l1;
-            l2 = l2 ? l2->next : l2;
+        int offset = 0;
+        ListNode *list1 = l1, *list2 = l2;
+        ListNode *result = new ListNode(0);
+        ListNode *prevNode = result;
+        
+        while(list1 != nullptr || list2 != nullptr || offset != 0){
+            int val1 = 0;
+            int val2 = 0;
+            if(list1 != nullptr)
+                val1 = list1 -> val;
+            if(list2 != nullptr)
+                val2 = list2 -> val;
+            
+            int number = val1 + val2 + offset;
+            offset = number / 10;
+            number = number % 10;
+            ListNode* newNode = new ListNode(number);
+            prevNode -> next = newNode;
+            prevNode = newNode;
+            if(list1 != nullptr)
+                list1 = list1 -> next;
+            if(list2 != nullptr)
+                list2 = list2 -> next;
         }
-        return preHead.next;
+        return result -> next;
     }
 };

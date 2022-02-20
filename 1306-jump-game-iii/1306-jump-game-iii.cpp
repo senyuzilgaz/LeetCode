805e570aaed896ef49e877eb1e395ecb500db27a
Ilgaz Şenyüz <61624665+senyuzilgaz@users.noreply.github.com>
2022-02-20 19:37:04 +0300

    Time: 36 ms (96.83%), Space: 35.5 MB (28.99%) - LeetHub

--- 1306-jump-game-iii/1306-jump-game-iii.cpp
+++ 1306-jump-game-iii/1306-jump-game-iii.cpp
@@ -7,18 +7,19 @@ public:
         
     }
     bool helper(vector<int>& arr, int start, vector<bool> &visited){
-        if(start >= arr.size())
+        if( start >= arr.size() )
             return false;
-        if(start < 0 )
+        if( start < 0)
             return false;
         if(visited[start] == true)
             return false;
         visited[start] = 1;
         
-        if(start < arr.size() && start >=0)
+        if( (start < arr.size()) && (start >=0)){
             return arr[start] == 0
                     || helper(arr, start + arr[start], visited)
                     || helper(arr, start - arr[start], visited);
+        }
         
         return false;        
     }

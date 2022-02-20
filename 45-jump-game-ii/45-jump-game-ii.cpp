4a5b8174698f8fb14cb2f0accd527ebca250efdf
Ilgaz Şenyüz <61624665+senyuzilgaz@users.noreply.github.com>
2022-02-20 18:58:44 +0300

    Time: 34 ms (51.18%), Space: 16.6 MB (60.86%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.cpp
+++ 45-jump-game-ii/45-jump-game-ii.cpp
@@ -3,10 +3,9 @@ public:
     int jump(vector<int>& nums) {
         int count = 0;
         int lastJumpIndex = 0;
-        int maxJumpIndex = 0;
-        if(nums.size() <= 1)
-            return  0;
-        
+        int maxJumpIndex = 0; 
+        if(nums.size() < 2)
+            return 0;
         for(int i = 0; i < nums.size(); ++i){
 
             maxJumpIndex = max(nums[i] + i, maxJumpIndex);

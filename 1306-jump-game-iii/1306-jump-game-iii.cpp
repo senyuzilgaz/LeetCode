59f971ef6533bc53f9295728b3fef9a6959afd11
Ilgaz Şenyüz <61624665+senyuzilgaz@users.noreply.github.com>
2022-02-20 19:35:39 +0300

    Time: 64 ms (49.78%), Space: 35.7 MB (26.26%) - LeetHub

--- 1306-jump-game-iii/1306-jump-game-iii.cpp
+++ 1306-jump-game-iii/1306-jump-game-iii.cpp
@@ -7,38 +7,20 @@ public:
         
     }
     bool helper(vector<int>& arr, int start, vector<bool> &visited){
-        if(start < 0)     
-            return false;
         if(start >= arr.size())
+            return false;
+        if(start < 0 )
             return false;
         if(visited[start] == true)
             return false;
-        visited[start] = true;
-        if(arr[start] == 0)
-            return true;
-        return helper(arr, start + arr[start], visited) || helper(arr, start - arr[start], visited);
+        visited[start] = 1;
+        
+        if(start < arr.size() && start >=0)
+            return arr[start] == 0
+                    || helper(arr, start + arr[start], visited)
+                    || helper(arr, start - arr[start], visited);
+        
+        return false;        
     }
 
-};/*
-public boolean canReach(int[] arr, int start) {
-        int[] visited = new int[arr.length];
-        return jumpGame(arr, start,visited); 
-    }
-     
-   public boolean jumpGame(int[] arr, int start,int[] visited)
-   {
-       if(start >= arr.length)
-            return false;
-        
-        if(start < 0)
-            return false;
-        
-        if(arr[start] == 0)
-            return true;
-        
-        if(visited[start] == 1)
-            return false;
-        
-        visited[start] = 1;       
-        return jumpGame(arr,start+arr[start],visited) || jumpGame(arr,start-arr[start],visited);
-   }*/
+};

e36a0a757afbe5c5a14ef4c8593f9dd9af362411
Ilgaz Şenyüz <61624665+senyuzilgaz@users.noreply.github.com>
2022-02-20 16:16:26 +0300

    Time: 89 ms (60.20%), Space: 48.2 MB (84.63%) - LeetHub

--- 55-jump-game/55-jump-game.cpp
+++ 55-jump-game/55-jump-game.cpp
@@ -2,10 +2,10 @@ class Solution {
 public:
     bool canJump(vector<int>& nums) {
         int distance = 0;
-        int fuel = nums.size() >= 1 ? nums[0]  : 0;
+        int fuel = nums.size() >= 1 ? nums[0] + 1 : 0;
         int i;
-        for(i = 0; i < nums.size() && fuel >= 0; ++i, --fuel){
-            fuel = max(fuel, nums[i]);
+        for(i = 0; i < nums.size() && fuel > 0; ++i, --fuel){
+            fuel = max(fuel, nums[i] + 1);
         }
         cout << i;
         return i >= nums.size();

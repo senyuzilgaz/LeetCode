e35682f257221a2b015edbe6a655b2c4ed24ac18
Ilgaz Şenyüz <61624665+senyuzilgaz@users.noreply.github.com>
2022-02-21 19:17:12 +0300

    Time: 8 ms (10.69%), Space: 6 MB (32.26%) - LeetHub

---
+++ 7-reverse-integer/7-reverse-integer.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int reverse(int x) {
+        long result = 0;
+        while(x){
+            result = 10*result + x%10;
+            x = x/10;
+            if(result < INT_MIN || result > INT_MAX)
+                return 0;
+        }
+        return result;
+    }
+};

ecc75e9e44a0a263718a90de8b124ff367c17912
Ilgaz Şenyüz <61624665+senyuzilgaz@users.noreply.github.com>
2022-10-04 19:52:11 +0300

    Time: 15 ms (35.46%), Space: 13.7 MB (81.53%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
@@ -4,11 +4,8 @@ public:
         int n = cost.size();
         if(n == 0 || n == 1)
             return 0;
-        if(n == 2)
-            return cost[0] >= cost[1] ? cost[1] : cost[0];
-        
-        int first = cost[0], second = cost[1];
-        
+
+        int first = cost[0], second = cost[1];        
         for(int i = 2; i < cost.size(); ++i){
             int res = cost[i] + min(first, second);
             first = second;
